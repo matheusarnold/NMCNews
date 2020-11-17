0b4e74efa2e8ef7b61a79dcfaba46fc67a2c8dc6
audreylrnt <audymillenia28@gmail.com>
2020-11-17 12:08:30 +0700

    continuation -ep3: placeholders, updating layout constraints, basic styling

---
+++ NMCNews/HomepageViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomepageViewController.swift
+//  NMCNews
+//
+//  Created by Laurentia Audrey on 17/11/20.
+//  Copyright © 2020 Matheus. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwind(_ sender:UIStoryboardSegue) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
